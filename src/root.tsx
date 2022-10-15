c536fd7ea08cfcb06df41c70b5464d677292b180
gasseklopper <m-morley@gmx.de>
2022-10-15 20:23:06 +0200

    update script declaration

--- src/root.tsx
+++ src/root.tsx
@@ -17,12 +17,6 @@ export default component$(() => {
         <meta charSet="utf-8" name="description" content="Halten Sie bei Ihren Meta Descriptions die Länge von 140 bis 160 Zeichen ein, damit Google sie vollständig anzeigt. Verwenden Sie auch Ihr Keyword!"/>
         <RouterHead />
 		<QwikPartytown forward={["dataLayer.push"]} />;
-
-        <script
-        async
-        type="text/partytown"
-        src="https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.4/dist/locomotive-scroll.min.js"
-        />
       </head>
       <body lang="en">
         <RouterOutlet />
