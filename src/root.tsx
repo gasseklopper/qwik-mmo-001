9f6071ae67032642d269d82b30cb3d6d1d833090
gasseklopper <m-morley@gmx.de>
2022-10-15 20:25:18 +0200

    delete partz town

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,6 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
-import { QwikPartytown } from './components/partytown/partytown';
 import './global.css';
 
 export default component$(() => {
@@ -16,7 +15,6 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" name="description" content="Halten Sie bei Ihren Meta Descriptions die Länge von 140 bis 160 Zeichen ein, damit Google sie vollständig anzeigt. Verwenden Sie auch Ihr Keyword!"/>
         <RouterHead />
-		<QwikPartytown forward={["dataLayer.push"]} />
       </head>
       <body lang="en">
         <RouterOutlet />
