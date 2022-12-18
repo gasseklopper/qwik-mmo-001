3c3703280afedadf3c70eb2d2fe32cdecf016f10
gasseklopper <m-morley@gmx.de>
2022-12-18 10:06:53 +0100

    add login fields

---
+++ src/components/QwikTextField/index.tsx
@@ -0,0 +1,46 @@
+import { component$, useStore } from '@builder.io/qwik';
+// import type { DocumentHead } from '@builder.io/qwik-city';
+import './style.css';
+// import { UserIcon, EnvelopeIcon, LockIcon, PeopleIcon, ManIcon } from '~/integrations/react/mui';
+// import { Man } from '@mui/icons-material';
+
+interface TextfieldProps {
+    name: String;
+    reference: any;
+}
+
+export default component$((props: TextfieldProps) => {
+
+    const state = useStore({
+        value: '',
+        isValid: true
+    });
+
+    const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+    // eslint-disable-next-line no-control-regex
+    const emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  return (
+    <div className='textfield' id='textfield'>
+        <input
+			ref={props.reference}
+			onInput$={()=> {
+			// eslint-disable-next-line qwik/valid-lexical-scope
+				state.value = props.reference.current?.value;
+			}}
+			className={
+				props.name === 'Username' || props.name === 'First Name' || props.name === 'Last Name' ?
+					(state.value.length === 0 ? ('') : (state.value.length > 3 ? 'valid' : 'invalid')) :
+					(props.name === 'Email' ? (state.value.length > 0 ? (state.value.match(emailReg) ? 'valid' : 'invalid') : '') : (state.value.length > 0 ? (state.value.match(passwordReg) ? 'valid' : 'invalid') : ('')))
+			}
+		/>
+        <span>
+			{props.name === 'Username' ? (<></>) : (props.name === 'First Name' ? (<></>) :
+			(props.name === 'Last Name' ? (<></>) : props.name === 'Password' || props.name === 'Confirm Password' ? (<></>) :
+			(<></>)))}
+		</span>
+        <label for='textfield'>{`${props.name}`}</label>
+        <div className='underline'></div>
+    </div>
+  );
+});
--- src/routes/input-context/index.tsx
+++ src/routes/input-context/index.tsx
@@ -1,10 +1,13 @@
-import { component$ } from '@builder.io/qwik'
+import { component$, useRef } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import InputContext from '~/components/inputContext/inputContext'
+import QwikTextField from '~/components/QwikTextField/index'
+// import './style.css';
 
 export default component$(() => {
 	return (
 		<>
+			<Login />
 			<InputContext />
 		</>
 	)
@@ -13,3 +16,20 @@ export default component$(() => {
 export const head: DocumentHead = {
 	title: 'Clock',
 }
+
+export const Login = component$(() => {
+
+  const username = useRef();
+  const password = useRef();
+
+  return (
+    <div className='container'>
+      <div className='input'>
+        <QwikTextField name='Username' reference={username}/>
+      </div>
+      <div className='input'>
+        <QwikTextField name='Password' reference={password}/>
+      </div>
+    </div>
+  );
+});
