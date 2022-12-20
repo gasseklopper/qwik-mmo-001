c49abfec1f1804ab66d1df8633a358cc72346209
gasseklopper <m-morley@gmx.de>
2022-12-21 00:26:24 +0100

    feat: add scss to input context

--- src/components/QwikTextField/index.tsx
+++ src/components/QwikTextField/index.tsx
@@ -1,23 +1,25 @@
-import { component$, useStore, useContext } from '@builder.io/qwik';
+import { component$, useStore, useContext, useStyles$ } from '@builder.io/qwik'
 // import type { DocumentHead } from '@builder.io/qwik-city';
-import { UserContext } from "~/components/inputContext/context";
-import './style.css';
+import { UserContext } from '~/components/inputContext/context'
+import styles from './style.scss'
 // import { UserIcon, EnvelopeIcon, LockIcon, PeopleIcon, ManIcon } from '~/integrations/react/mui';
 // import { Man } from '@mui/icons-material';
 
 interface TextfieldProps {
-    name: String;
-    reference: any;
+	name: String
+	reference: any
 }
 
 export default component$((props: TextfieldProps) => {
+	useStyles$(styles)
 	const user = useContext(UserContext)
 
-    const state = useStore({
-        value: '',
-        isValid: true,
-		animateValid: true
-    });
+	const state = useStore({
+		value: '',
+		isValid: true,
+		animateValid: true,
+		autoFocus: true,
+	})
 
 	if (props.name === 'Username') {
 		user.username = state.value
@@ -40,87 +42,109 @@ export default component$((props: TextfieldProps) => {
 	}
 
 	// min 4 chars number or chars
-    const standradReg = /[0-9a-zA-Z]{4,}/;
+	const standradReg = /[0-9a-zA-Z]{4,}/
 
-    const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-    // eslint-disable-next-line no-control-regex
-    const emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+	const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/
+
+	const emailReg =
+		// eslint-disable-next-line no-control-regex
+		/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 	return (
 		<>
-			{props.name === 'Password' || props.name === 'Email' || props.name === 'Confirm Password' ?
+			{props.name === 'Password' ||
+			props.name === 'Email' ||
+			props.name === 'Confirm Password' ? (
 				<>
-					<div className='textfield' id='textfield'>
+					<div className="textfield" id="textfield">
 						<input
-								ref={props.reference}
-								onInput$={()=> {
-									state.animateValid = true;
-									// eslint-disable-next-line qwik/valid-lexical-scope
-									state.value = props.reference.current?.value;
-									// {state.value.length === 0 ? (state.animateValid = false) : (state.value.match(standradReg) ? (state.animateValid = false) : (state.animateValid = true))}
-									setTimeout(() => {
-										state.animateValid = false
-										// state.animateValid = true
-									}, 600);
-								}}
-								className={
-									props.name === 'Email' ?
-									(state.value.length > 0 ? (state.value.match(emailReg) ? 'valid' : (state.animateValid === false ?
-										'invalid' :
-										'invalid anim')) : '') : (state.value.length > 0 ? (state.value.match(passwordReg) ? 'valid' : (state.animateValid === false ?
-										'invalid' :
-										'invalid anim')) :
-									(''))
-								}
+							ref={props.reference}
+							onInput$={() => {
+								state.animateValid = true
+								// eslint-disable-next-line qwik/valid-lexical-scope
+								state.value = props.reference.current?.value
+								// {state.value.length === 0 ? (state.animateValid = false) : (state.value.match(standradReg) ? (state.animateValid = false) : (state.animateValid = true))}
+								setTimeout(() => {
+									state.animateValid = false
+									// state.animateValid = true
+								}, 600)
+							}}
+							className={
+								props.name === 'Email'
+									? state.value.length > 0
+										? state.value.match(emailReg)
+											? 'valid'
+											: state.animateValid === false
+											? 'invalid'
+											: 'invalid anim'
+										: ''
+									: state.value.length > 0
+									? state.value.match(passwordReg)
+										? 'valid'
+										: state.animateValid === false
+										? 'invalid'
+										: 'invalid anim'
+									: ''
+							}
+							autoFocus={state.autoFocus}
 						/>
 						<span className={'icon'}>
-							{ props.name === 'Password' || props.name === 'Confirm Password' ? (<></>) :
-							(<></>)}
+							{props.name === 'Password' ||
+							props.name === 'Confirm Password' ? (
+								<></>
+							) : (
+								<></>
+							)}
 						</span>
-						<label for='textfield'>{`${props.name}`}</label>
-						<div className='underline'></div>
+						<label for="textfield">{`${props.name}`}</label>
+						<div className="underline"></div>
 					</div>
-
-				</>:
-				null
-			}
-			{props.name === 'Username' || props.name === 'First Name' || props.name === 'Last Name' ?
+				</>
+			) : null}
+			{props.name === 'Username' ||
+			props.name === 'First Name' ||
+			props.name === 'Last Name' ? (
 				<>
-					<div className='textfield' id='textfield'>
+					<div className="textfield" id="textfield">
 						<input
-								ref={props.reference}
-								onInput$={()=> {
-									state.animateValid = true;
-									// eslint-disable-next-line qwik/valid-lexical-scope
-									state.value = props.reference.current?.value;
-									// {state.value.length === 0 ? (state.animateValid = false) : (state.value.match(standradReg) ? (state.animateValid = false) : (state.animateValid = true))}
-									setTimeout(() => {
-										state.animateValid = false
-										// state.animateValid = true
-									}, 600);
-								}}
-								className={
-									state.value.length === 0 ?
-									('') :
-									(state.value.match(standradReg) ?
-										'valid' :
-										(state.animateValid === false ?
-											'invalid' :
-											'invalid anim')
-									)
-								}
+							ref={props.reference}
+							onInput$={() => {
+								state.animateValid = true
+								// eslint-disable-next-line qwik/valid-lexical-scope
+								state.value = props.reference.current?.value
+								// {state.value.length === 0 ? (state.animateValid = false) : (state.value.match(standradReg) ? (state.animateValid = false) : (state.animateValid = true))}
+								setTimeout(() => {
+									state.animateValid = false
+									// state.animateValid = true
+								}, 600)
+							}}
+							className={
+								state.value.length === 0
+									? ''
+									: state.value.match(standradReg)
+									? 'valid'
+									: state.animateValid === false
+									? 'invalid'
+									: 'invalid anim'
+							}
+							autoFocus={state.autoFocus}
 						/>
 						<span className={'icon'}>
-							{props.name === 'Username' ? (<></>) : (props.name === 'First Name' ? (<></>) :
-							(props.name === 'Last Name' ? (<></>) : (<></>)))}
+							{props.name === 'Username' ? (
+								<></>
+							) : props.name === 'First Name' ? (
+								<></>
+							) : props.name === 'Last Name' ? (
+								<></>
+							) : (
+								<></>
+							)}
 						</span>
-						<label for='textfield'>{`${props.name}`}</label>
-						<div className='underline'></div>
+						<label for="textfield">{`${props.name}`}</label>
+						<div className="underline"></div>
 					</div>
-
-				</>:
-				null
-			}
+				</>
+			) : null}
 		</>
-	);
-});
+	)
+})
