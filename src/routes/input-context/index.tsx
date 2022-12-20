6a32e074da168de5f075600fd07a7619d87af3b7
gasseklopper <m-morley@gmx.de>
2022-12-21 00:53:47 +0100

    feat: update base styling

--- src/routes/input-context/index.tsx
+++ src/routes/input-context/index.tsx
@@ -1,13 +1,13 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 import InputContext from '~/components/inputContext/inputContext'
-// import './style.css';
+import './style.css'
 
 export default component$(() => {
 	return (
-		<>
+		<div className="input-context">
 			<InputContext />
-		</>
+		</div>
 	)
 })
 
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,10 @@
 import { component$, Slot, useStyles$ } from '@builder.io/qwik'
 import Footer from '~/components/footer/footer'
 import Header from '~/components/header/header'
-import styles from '~/index.scss';
+import styles from '~/index.scss'
 
 export default component$(() => {
-	useStyles$(styles);
+	useStyles$(styles)
 	return (
 		<>
 			<Header />
