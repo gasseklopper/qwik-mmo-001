161791c89ad72b82b74afc3433f6c7d88e2d15ac
gasseklopper <m-morley@gmx.de>
2022-12-24 21:24:16 +0100

    feat: update content

--- src/routes/use-hooks/index.tsx
+++ src/routes/use-hooks/index.tsx
@@ -3,6 +3,8 @@ import type { DocumentHead } from '@builder.io/qwik-city'
 import UsingContext from '~/components/usingContext/usingContext'
 import UseStyles from '~/components/useStyles/useStyles'
 import UseReference from '~/components/useReference/useReference'
+import Counter from '~/components/counter/counter'
+import Debounced from '~/components/debounced/debounced'
 
 export default component$(() => {
 	return (
@@ -10,6 +12,8 @@ export default component$(() => {
 			<UsingContext />
 			<UseStyles />
 			<UseReference />
+			<Counter />
+			<Debounced />
 		</>
 	)
 })
