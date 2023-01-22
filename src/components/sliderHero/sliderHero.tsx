57b0a1b55cfae35be1bbe7062347fbb41bcde5be
gasseklopper <m-morley@gmx.de>
2023-01-22 14:14:24 +0100

    feat: update slider

--- src/components/sliderHero/sliderHero.tsx
+++ src/components/sliderHero/sliderHero.tsx
@@ -1,9 +1,19 @@
-import { component$, useStore, $ } from '@builder.io/qwik'
+import { component$, useStore, $, useStyles$ } from '@builder.io/qwik'
+import styles from './sliderHero.scss?inline'
 
 export default component$(() => {
-	const store = useStore({
+	useStyles$(styles)
+
+	const sliderConfig = useStore({
 		indexId: 0,
-		imgUrl: '../../assets/images/photography/black/Template_index_01.jpg',
+		sliderItems: 19,
+		activeAutoslide: true,
+		durationAutoslide: 2000,
+	})
+
+	const imageStore = useStore({
+		id: 0,
+		imgUrl: 'https://via.placeholder.com/941x557',
 		imgWidth: 941,
 		imgHeight: 557,
 		imgAlt: 'hero image',
@@ -152,66 +162,95 @@ export default component$(() => {
 		},
 	]
 
+	if (images.length <= 1) {
+		console.log(images.length)
+	}
+
+	const startAutoPlay = $(() => {
+		sliderConfig.activeAutoslide = true
+		const timerId = setInterval(function tick() {
+			if (!sliderConfig.activeAutoslide) {
+				clearInterval(timerId)
+			} else {
+				if (sliderConfig.indexId === sliderConfig.sliderItems) {
+					sliderConfig.indexId = 0
+				} else {
+					sliderConfig.indexId++
+				}
+				imageStore.imgUrl = images[sliderConfig.indexId].imgUrl
+			}
+		}, sliderConfig.durationAutoslide)
+	})
+
+	const stopAutoPlay = $(() => {
+		sliderConfig.activeAutoslide = false
+	})
+
 	const indexCheckIncrement = $(() => {
-		if (store.indexId === 19) {
-			store.indexId = 0
+		if (sliderConfig.indexId === sliderConfig.sliderItems) {
+			sliderConfig.indexId = 0
 		} else {
-			store.indexId++
+			sliderConfig.indexId++
 		}
-		store.imgUrl = images[store.indexId].imgUrl
+		imageStore.imgUrl = images[sliderConfig.indexId].imgUrl
 	})
 
 	const indexCheckDecrement = $(() => {
-		if (store.indexId === 0) {
-			store.indexId = 19
+		if (sliderConfig.indexId === 0) {
+			sliderConfig.indexId = sliderConfig.sliderItems
 		} else {
-			store.indexId--
+			sliderConfig.indexId--
 		}
-		store.imgUrl = images[store.indexId].imgUrl
+		imageStore.imgUrl = images[sliderConfig.indexId].imgUrl
 	})
 
 	return (
 		<div>
 			<h1 class="h1">qwik SLIDER</h1>
 			<div class="slider">
-				<div class="controls">
-					<div class="next">
+				<div class="slider__controls">
+					<div class="slider__next">
 						<button onClick$={indexCheckIncrement}>next</button>
 					</div>
-					<div class="previous">
+					<div class="slider__previous">
 						<button onClick$={indexCheckDecrement}>previous</button>
 					</div>
+					<button onClick$={startAutoPlay} id="start">
+						Start
+					</button>
+					<button onClick$={stopAutoPlay} id="stop">
+						Stop
+					</button>
+					<div class="slider__overview">
+						{images.map((_items: any, itemIndex: number) => {
+							if (itemIndex <= sliderConfig.sliderItems) {
+								return (
+									<button
+										key={itemIndex}
+										onClick$={() => (
+											(sliderConfig.indexId = itemIndex),
+											(imageStore.imgUrl =
+												images[itemIndex].imgUrl)
+										)}
+										class={
+											sliderConfig.indexId === itemIndex
+												? 'slider__overview-item slider__overview-item--active'
+												: 'slider__overview-item'
+										}
+									></button>
+								)
+							}
+						})}
+					</div>
 				</div>
-				<div class="images">
-					<div class="item">
-						<img src="" alt="" class="image" />
+				<div class="slider__images">
+					<div class="slider__item">
+						<img src="" alt="" class="slider__image" />
 						<img
-							src={store.imgUrl}
-							// width={store.imgWidth}
-							// height={store.imgHeight}
-							alt={store.imgAlt}
-						/>
-					</div>
-				</div>
-			</div>
-			<h1 class="h1">qwik SLIDER</h1>
-			<div class="slider">
-				<div class="controls">
-					<div class="next">
-						<button onClick$={indexCheckIncrement}>next</button>
-					</div>
-					<div class="previous">
-						<button onClick$={indexCheckDecrement}>previous</button>
-					</div>
-				</div>
-				<div class="images">
-					<div class="item">
-						<img src="" alt="" class="image" />
-						<img
-							src={store.imgUrl}
-							width={store.imgWidth}
-							height={store.imgHeight}
-							alt={store.imgAlt}
+							src={imageStore.imgUrl}
+							width={imageStore.imgWidth}
+							height={imageStore.imgHeight}
+							alt={imageStore.imgAlt}
 						/>
 					</div>
 				</div>
