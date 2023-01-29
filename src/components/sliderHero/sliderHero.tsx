5961ceff9d9210386b093288e770247a63ee792f
gasseklopper <m-morley@gmx.de>
2023-01-29 01:17:31 +0100

    update

--- src/components/sliderHero/sliderHero.tsx
+++ src/components/sliderHero/sliderHero.tsx
@@ -305,14 +305,156 @@ export default component$(() => {
 export const TestSlider = component$((props) => {
 	useClientEffect$(() => {
 		gsap.registerPlugin(Observer)
-		Observer.create({
-			type: 'wheel,touch,pointer',
-			onDown: () => console.log('test1'),
-			onUp: () => console.log('test2'),
-			// invert the mouse wheel delta
-			wheelSpeed: -1,
-			tolerance: 10,
-		})
+
+		const setCurrentSlide = (position) => {
+			console.log('set current slide')
+			// if (current !== -1) {
+			// 	slidesArr[current].DOM.el.classList.remove('slide--current')
+			// }
+			// current = position
+			// slidesArr[current].DOM.el.classList.add('slide--current')
+			// DOM.navigationItems[current].classList.add(
+			// 	'frame__nav-button--current'
+			// )
+		}
+
+		const navigate = (newPosition) => {
+			// isAnimating = true
+			// // change navigation current class
+			// DOM.navigationItems[current].classList.remove(
+			// 	'frame__nav-button--current'
+			// )
+			// DOM.navigationItems[newPosition].classList.add(
+			// 	'frame__nav-button--current'
+			// )
+			// // navigation direction
+			// const direction =
+			// 	current < newPosition
+			// 		? current === 0 && newPosition === totalSlides - 1
+			// 			? 'prev'
+			// 			: 'next'
+			// 		: current === totalSlides - 1 && newPosition === 0
+			// 		? 'next'
+			// 		: 'prev'
+			// const currentSlide = slidesArr[current]
+			// current = newPosition
+			// const upcomingSlide = slidesArr[current]
+			// gsap.timeline({
+			// 	defaults: {
+			// 		duration: 1.6,
+			// 		ease: 'power3.inOut',
+			// 	},
+			// 	onComplete: () => {
+			// 		currentSlide.DOM.el.classList.remove('slide--current')
+			// 		// Close the current slide if it was open
+			// 		if (currentSlide.isOpen) {
+			// 			hideContent(currentSlide)
+			// 		}
+			// 		isAnimating = false
+			// 	},
+			// })
+			// 	.addLabel('start', 0)
+			// 	.set(
+			// 		[currentSlide.DOM.imgInner, upcomingSlide.DOM.imgInner],
+			// 		{
+			// 			transformOrigin:
+			// 				direction === 'next' ? '50% 0%' : '50% 100%',
+			// 		},
+			// 		'start'
+			// 	)
+			// 	// Place coming slide either above (translate -100%) or below (translate 100%) and the slide__inner to the opposite translate.
+			// 	.set(
+			// 		upcomingSlide.DOM.el,
+			// 		{
+			// 			yPercent: direction === 'next' ? 100 : -100,
+			// 		},
+			// 		'start'
+			// 	)
+			// 	.set(
+			// 		upcomingSlide.DOM.inner,
+			// 		{
+			// 			yPercent: direction === 'next' ? -100 : 100,
+			// 		},
+			// 		'start'
+			// 	)
+			// 	// Add current class
+			// 	.add(() => {
+			// 		upcomingSlide.DOM.el.classList.add('slide--current')
+			// 	}, 'start')
+			// 	// hide the back button and show back the cursor text if the current slide was open
+			// 	.add(() => {
+			// 		if (currentSlide.isOpen) {
+			// 			toggleCursorBackTexts()
+			// 		}
+			// 	}, 'start')
+			// 	// Current slide moves either up or down (translate 100% or -100%)
+			// 	.to(
+			// 		currentSlide.DOM.el,
+			// 		{
+			// 			yPercent: direction === 'next' ? -100 : 100,
+			// 		},
+			// 		'start'
+			// 	)
+			// 	.to(
+			// 		currentSlide.DOM.imgInner,
+			// 		{
+			// 			scaleY: 2,
+			// 		},
+			// 		'start'
+			// 	)
+			// 	// Upcoming slide translates to 0
+			// 	.to(
+			// 		[upcomingSlide.DOM.el, upcomingSlide.DOM.inner],
+			// 		{
+			// 			yPercent: 0,
+			// 		},
+			// 		'start'
+			// 	)
+			// 	.to(
+			// 		upcomingSlide.DOM.imgInner,
+			// 		{
+			// 			ease: 'power2.inOut',
+			// 			startAt: { scaleY: 2 },
+			// 			scaleY: 1,
+			// 		},
+			// 		'start'
+			// 	)
+		}
+
+		const next = () => {
+			// const newPosition = current < totalSlides - 1 ? current + 1 : 0
+			const newPosition = 1
+			navigate(newPosition)
+		}
+
+		const prev = () => {
+			//   const newPosition = current > 0 ? current - 1 : totalSlides - 1
+			const newPosition = 2
+			navigate(newPosition)
+		}
+
+		const initEvents = () => {
+			Observer.create({
+				type: 'wheel,touch,pointer',
+				onDown: () => {
+					console.log('prev')
+					prev()
+				},
+				onUp: () => {
+					console.log('next')
+					next()
+				},
+				// invert the mouse wheel delta
+				wheelSpeed: -1,
+				tolerance: 10,
+			})
+		}
+
+		// Set current slide
+		setCurrentSlide(0)
+
+		// Initialize the events
+		initEvents()
 	})
 	return (
 		<div class="slides">
