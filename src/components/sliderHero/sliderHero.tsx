448f51b0f6f3126b6efe66c19c894c1866cab1a1
gasseklopper <m-morley@gmx.de>
2023-02-04 19:00:18 +0100

    add: animations and initial slide

--- src/components/sliderHero/sliderHero.tsx
+++ src/components/sliderHero/sliderHero.tsx
@@ -19,13 +19,235 @@ export default component$(() => {
 	)
 })
 
-// export const TestSlider = component$((props) => {
+export const images = [
+	{
+		id: 1,
+		imgUrl: '../../assets/images/photography/black/Template_index_01.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		imgAlt: 'hero image',
+		content: {
+			headline: 'headline 001',
+			bodytext:
+				'lorem 1 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 2,
+		imgUrl: '../../assets/images/photography/black/Template_index_02.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 002',
+			bodytext:
+				'lorem 2 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 3,
+		imgUrl: '../../assets/images/photography/black/Template_index_03.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 003',
+			bodytext:
+				'lorem 3 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 4,
+		imgUrl: '../../assets/images/photography/black/Template_index_04.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 004',
+			bodytext:
+				'lorem 4 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 5,
+		imgUrl: '../../assets/images/photography/black/Template_index_05.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 005',
+			bodytext:
+				'lorem 5 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 6,
+		imgUrl: '../../assets/images/photography/black/Template_index_06.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 006',
+			bodytext:
+				'lorem 6 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 7,
+		imgUrl: '../../assets/images/photography/black/Template_index_07.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 007',
+			bodytext:
+				'lorem 7 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 8,
+		imgUrl: '../../assets/images/photography/black/Template_index_08.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 008',
+			bodytext:
+				'lorem 8 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 9,
+		imgUrl: '../../assets/images/photography/black/Template_index_09.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 009',
+			bodytext:
+				'lorem 9 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 10,
+		imgUrl: '../../assets/images/photography/black/Template_index_010.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 010',
+			bodytext:
+				'lorem 10 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 11,
+		imgUrl: '../../assets/images/photography/black/Template_index_011.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 011',
+			bodytext:
+				'lorem 11 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 12,
+		imgUrl: '../../assets/images/photography/black/Template_index_012.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 012',
+			bodytext:
+				'lorem 12 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 13,
+		imgUrl: '../../assets/images/photography/black/Template_index_013.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 013',
+			bodytext:
+				'lorem 13 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 14,
+		imgUrl: '../../assets/images/photography/black/Template_index_014.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 014',
+			bodytext:
+				'lorem 14 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 15,
+		imgUrl: '../../assets/images/photography/black/Template_index_015.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 015',
+			bodytext:
+				'lorem 15 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 16,
+		imgUrl: '../../assets/images/photography/black/Template_index_016.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 016',
+			bodytext:
+				'lorem 16 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 17,
+		imgUrl: '../../assets/images/photography/black/Template_index_017.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 017',
+			bodytext:
+				'lorem 17 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 18,
+		imgUrl: '../../assets/images/photography/black/Template_index_018.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 018',
+			bodytext:
+				'lorem 18 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 19,
+		imgUrl: '../../assets/images/photography/black/Template_index_019.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 019',
+			bodytext:
+				'lorem 19 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+	{
+		id: 20,
+		imgUrl: '../../assets/images/photography/black/Template_index_020.jpg',
+		imgWidth: 941,
+		imgHeight: 557,
+		content: {
+			headline: 'headline 020',
+			bodytext:
+				'lorem 20 ----- Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vero quasi minus natus rem, quas et omnis incidunt harum qui? Corporis et incidunt minima amet animi suscipit sequi ab, mollitia cum necessitatibus, maiores aperiam harum nisi quia quo, totam voluptatibus sed quidem. Id, earum magni.',
+		},
+	},
+]
+
 export const TestSlider = component$(() => {
 	useStyles$(css)
 
 	const sliderConfig = useStore({
 		indexId: 0,
-		sliderItems: 2,
 		width: 941,
 		isAnimating: false,
 		current: -1,
@@ -53,153 +275,11 @@ export const TestSlider = component$(() => {
 		imageStore.id = sliderConfig.indexId
 	})
 
-	const images = [
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_01.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_02.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_03.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_04.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_05.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_06.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_07.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_08.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_09.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_010.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_011.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_012.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_013.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_014.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_015.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_016.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_017.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_018.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_019.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-		{
-			id: 1,
-			imgUrl: '../../assets/images/photography/black/Template_index_020.jpg',
-			imgWidth: 941,
-			imgHeight: 557,
-			imgAlt: 'hero image',
-		},
-	]
-
 	const refSlides = useSignal<HTMLDivElement>()
 
 	useClientEffect$(() => {
 		gsap.registerPlugin(Observer)
+
 		// total number of slides
 		const totalSlides = refSlides.value!.querySelectorAll('.slide').length
 
@@ -208,13 +288,6 @@ export const TestSlider = component$(() => {
 			slidesArr.push(slide)
 		})
 
-		console.log('totalSlides', totalSlides)
-		console.log('slidesArr', slidesArr)
-
-		// current slide position
-		// let current = -1
-
-		// check if animation is in progress
 		sliderConfig.isAnimating = false
 
 		const setCurrentSlide = (position: number) => {
@@ -224,10 +297,10 @@ export const TestSlider = component$(() => {
 				)
 			}
 			sliderConfig.current = position
-			slidesArr[sliderConfig.current].classList.add('slide--current')
 		}
 
 		const next = () => {
+			console.log('next sliderConfig.current', sliderConfig.current)
 			const newPosition =
 				sliderConfig.current < totalSlides - 1
 					? sliderConfig.current + 1
@@ -236,10 +309,11 @@ export const TestSlider = component$(() => {
 		}
 
 		const prev = () => {
+			console.log('sliderConfig.current', sliderConfig.current)
 			const newPosition =
-				sliderConfig.current < totalSlides - 1
-					? sliderConfig.current + 1
-					: 0
+				sliderConfig.current === 0 || sliderConfig.current === -1
+					? totalSlides - 1
+					: sliderConfig.current - 1
 			navigate(newPosition)
 		}
 
@@ -264,8 +338,7 @@ export const TestSlider = component$(() => {
 
 			gsap.timeline({
 				defaults: {
-					duration: 2.6,
-					ease: 'power3.inOut',
+					duration: 1.1,
 				},
 				onComplete: () => {
 					currentSlide.classList.remove('slide--current')
@@ -276,17 +349,17 @@ export const TestSlider = component$(() => {
 			})
 				.addLabel('start', 0)
 
-				.set(
-					[
-						currentSlide.querySelector('.slide__inner'),
-						upcomingSlide.querySelector('.slide__inner'),
-					],
-					{
-						transformOrigin:
-							direction === 'next' ? '50% 0%' : '50% 100%',
-					},
-					'start'
-				)
+				// .set(
+				// 	[
+				// 		currentSlide.querySelector('.slide__inner'),
+				// 		upcomingSlide.querySelector('.slide__inner'),
+				// 	],
+				// 	{
+				// 		transformOrigin:
+				// 			direction === 'next' ? '50% 0%' : '50% 100%',
+				// 	},
+				// 	'start'
+				// )
 				// Place coming slide either above (translate -100%) or below (translate 100%) and the slide__inner to the opposite translate.
 				// .set(
 				// 	upcomingSlide.querySelector('.slide'),
@@ -333,9 +406,9 @@ export const TestSlider = component$(() => {
 				// 	'start'
 				// )
 				.to(
-					upcomingSlide.querySelector('.slide__img-inner'),
+					upcomingSlide.querySelector('.slide__inner'),
 					{
-						ease: 'power2.inOut',
+						ease: 'power4.inOut',
 						startAt: { yPercent: 100 },
 						yPercent: 0,
 					},
@@ -383,25 +456,51 @@ export const TestSlider = component$(() => {
 			<div class="slider__previous">
 				<button onClick$={indexCheckDecrement}>previous</button>
 			</div>
-
+			<div class="navigation">
+				{images.map((item: any, itemIndex: number) => {
+					if (itemIndex === sliderConfig.current - 1) {
+						return (
+							<div class="navigation__item" key={itemIndex}>
+								<p>{item.content.headline}</p>
+							</div>
+						)
+					}
+					if (itemIndex === sliderConfig.current + 1) {
+						return (
+							<div class="navigation__item" key={itemIndex}>
+								<p>{item.content.headline}</p>
+							</div>
+						)
+					}
+					if (itemIndex === sliderConfig.current) {
+						return (
+							<div class="navigation__item" key={itemIndex}>
+								<p>{item.content.headline}</p>
+							</div>
+						)
+					}
+				})}
+			</div>
 			<div class="slides" ref={refSlides}>
 				{images.map((item: any, itemIndex: number) => {
 					return (
-						<div class="slide" key={itemIndex}>
+						<div
+							class={
+								itemIndex !== 0
+									? 'slide'
+									: 'slide slide--current '
+							}
+							key={itemIndex}
+						>
 							<div class="slide__inner">
-								{/* <div class="slide__content">
+								<div class="slide__content">
 									<div
 										class="slide__content-img"
 										style={`background-image: url(${item.imgUrl});`}
 									></div>
-									<h2>No choice</h2>
-									<p>
-										Fall into line, you will do as you’re
-										told. No choice fatigue, your blood is
-										running cold. We lose control, the world
-										will fall apart.
-									</p>
-								</div> */}
+									<h2>{item.content.headline}</h2>
+									<p>{item.content.bodytext}</p>
+								</div>
 								<div class="slide__img">
 									<div
 										class="slide__img-inner"
