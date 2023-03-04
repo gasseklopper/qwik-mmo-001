f43dbe02a69ad4bdda6405977a6213e8020e2ae2
gasseklopper <m-morley@gmx.de>
2023-03-04 13:46:21 +0100

    feat: update api keys

--- src/utils/supabase copy.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supaBaseUrl = 'https://clvpdjstdoutmbtoydud.supabase.co'
-const anonPublic =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsdnBkanN0ZG91dG1idG95ZHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc5MzI4NzQsImV4cCI6MTk5MzUwODg3NH0.RE0LpfmTzmRRrZZZ0QsS9Z6YNrEXihRWeQHIxvZkZrY'
-
-export const supabase = createClient(supaBaseUrl, anonPublic)
--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supaBaseUrl = 'https://imnerbgpuvvqkfwggrzu.supabase.co'
+const supaBaseUrl = 'https://clvpdjstdoutmbtoydud.supabase.co'
 const anonPublic =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltbmVyYmdwdXZ2cWtmd2dncnp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc3NjEyMzIsImV4cCI6MTk5MzMzNzIzMn0.29NBSA4WTOrcEW4-WQ_-32MWrrmxy6112PHwKmRXTxM'
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsdnBkanN0ZG91dG1idG95ZHVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc5MzI4NzQsImV4cCI6MTk5MzUwODg3NH0.RE0LpfmTzmRRrZZZ0QsS9Z6YNrEXihRWeQHIxvZkZrY'
 
 export const supabase = createClient(supaBaseUrl, anonPublic)
