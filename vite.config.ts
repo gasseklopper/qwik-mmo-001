12cbe568792e87dc84f6c6ab22217124268e7b69
gasseklopper <m-morley@gmx.de>
2023-03-17 13:33:44 +0100

    feat: cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import netlifyEdge from '@netlify/vite-plugin-netlify-edge'
 import { partytownVite } from '@builder.io/partytown/utils'
 import { join } from 'path'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin/'
 
 export default defineConfig(() => {
 	return {
-		optimizeDeps: {
-			exclude: [
-				// I tried pretty much everything here: no way to force vite pre-bundling to ignore it...
-				'styled-vanilla-extract/qwik',
-			],
-		},
 		plugins: [
 			qwikCity(),
 			qwikVite({
@@ -23,7 +16,6 @@ export default defineConfig(() => {
 			tsconfigPaths(),
 			netlifyEdge({ functionName: 'entry.netlify-edge' }),
 			partytownVite({ dest: join(__dirname, 'public', '~partytown') }),
-			vanillaExtractPlugin(),
 		],
 		test: {}, // this is the config entry we are adding
 		// esbuild: {
