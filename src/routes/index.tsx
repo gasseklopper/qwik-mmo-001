61691de7aafe7e41d5d77a69d5a15b3e5a4c7f78
gasseklopper <m-morley@gmx.de>
2023-03-18 21:26:56 +0100

    delte slider hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
-import SliderHero from '~/components/sliderHero/sliderHero'
 
 export default component$(() => {
 	return (
 		<>
-			<SliderHero></SliderHero>
+			{/* <SliderHero></SliderHero> */}
+			slider hero
 		</>
 	)
 })
