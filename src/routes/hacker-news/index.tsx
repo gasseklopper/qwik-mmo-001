e62ef3e2e33aacd9b004a29b4440e417c2b1ea79
gasseklopper <m-morley@gmx.de>
2023-06-10 17:08:03 +0200

    feat: disable hacker news

--- src/routes/hacker-news/index.tsx
+++ src/routes/hacker-news/index.tsx
@@ -1,13 +1,11 @@
 import { component$ } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
-import HackerNews from '~/components/hackerNews/hackerNews'
+// import HackerNews from '~/components/hackerNews/hackerNews'
 
 export default component$(() => {
 	return (
 		<div class="row">
-			<div class="column">
-				<HackerNews />
-			</div>
+			<div class="column">{/* <HackerNews /> */}</div>
 		</div>
 	)
 })
