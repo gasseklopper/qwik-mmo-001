fc05e8c7de1e80dfaa5e86a8398001bb08432ae8
gasseklopper <m-morley@gmx.de>
2023-06-19 16:30:55 +0200

    update use ref

--- src/components/useReference/useReference.tsx
+++ src/components/useReference/useReference.tsx
@@ -1,32 +1,36 @@
 import {
 	component$,
 	useVisibleTask$,
-	// useStore,
 	useSignal,
+	useStore,
 } from '@builder.io/qwik'
 
 export default component$(() => {
-	const width = useSignal(12)
-	const height = useSignal(12)
+	const store = useStore({
+		width: 0,
+		height: 0,
+	})
 
 	const outputRef = useSignal<Element>()
 	useVisibleTask$(() => {
-		console.log('test', outputRef.value)
 		if (outputRef.value) {
 			const rect = outputRef.value.getBoundingClientRect()
-			width.value = Math.round(rect.width)
-			height.value = Math.round(rect.height)
+			store.width = Math.round(rect.width)
+			store.height = Math.round(rect.height)
 		}
 	})
 
 	return (
 		<main>
-			<aside style={{ border: '1px solid black', width: '100px' }}>
+			<aside
+				style={{ border: '1px solid black', width: '100px' }}
+				ref={outputRef}
+			>
 				Change text value here to stretch the box.
 			</aside>
 			<p>
-				The above red box is {height.value} pixels high and{' '}
-				{width.value} pixels wide.
+				The above red box is {store.height} pixels high and{' '}
+				{store.width} pixels wide.
 			</p>
 		</main>
 	)
