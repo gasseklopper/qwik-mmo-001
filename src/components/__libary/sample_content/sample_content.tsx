088dbbe6cdafdfd71fe1ce328d753cb8d834a08b
gasseklopper <gasseklopper@users.noreply.github.com>
2023-12-31 03:05:04 +0100

    feat; added miami color sheme

--- src/components/__libary/sample_content/sample_content.tsx
+++ src/components/__libary/sample_content/sample_content.tsx
@@ -4,24 +4,42 @@ export default component$(() => {
 	return (
 		<section>
 			<div class="row">
-				<div class="column surface1">
-					<h1 class="h1">Sample COntent Grid color shema</h1>
+				<div class="column large-6 huge-6 primary ">
+					<h1 class="h1"> primary Sample COntent Grid color shema</h1>
 					<p>
 						Praesent ac adipiscing ullamcorper semper ut amet ac
 						risus. Lorem sapien ut odio odio nunc. Ac adipiscing
 						nibh porttitor erat risus justo adipiscing adipiscing
-						amet placerat accumsan. Vis. Faucibus odio magna tempus
-						adipiscing a non. In mi primis arcu ut non accumsan
-						vivamus ac blandit adipiscing adipiscing arcu metus
-						praesent turpis eu ac lacinia nunc ac commodo gravida
-						adipiscing eget accumsan ac nunc adipiscing adipiscing
-						lorem ipsum dolor sit amet nullam veroeros adipiscing.
+					</p>
+				</div>
+				<div class="column large-6 huge-6 secondary">
+					<h1 class="h1"> secondary Sample COntent Grid color shema</h1>
+					<p>
+						Praesent ac adipiscing ullamcorper semper ut amet ac
+						risus. Lorem sapien ut odio odio nunc. Ac adipiscing
+						nibh porttitor erat risus justo adipiscing adipiscing
+					</p>
+				</div>
+				<div class="column large-6 huge-6 tertiary">
+					<h1 class="h1"> tertiary Sample COntent Grid color shema</h1>
+					<p>
+						Praesent ac adipiscing ullamcorper semper ut amet ac
+						risus. Lorem sapien ut odio odio nunc. Ac adipiscing
+						nibh porttitor erat risus justo adipiscing adipiscing
+					</p>
+				</div>
+				<div class="column large-6 huge-6 quaternary">
+					<h1 class="h1"> quaternary Sample COntent Grid color shema</h1>
+					<p>
+						Praesent ac adipiscing ullamcorper semper ut amet ac
+						risus. Lorem sapien ut odio odio nunc. Ac adipiscing
+						nibh porttitor erat risus justo adipiscing adipiscing
 					</p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="column large-4 huge-4 surface1">
-					<h2 class="h1">surface1</h2>
+					<h3 class="h2">surface1</h3>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
 						gravida odio porttitor sem non mi integer non faucibus
@@ -34,7 +52,7 @@ export default component$(() => {
 					</p>
 				</div>
 				<div class="column large-4 huge-4 surface2">
-					<h3 class="h2">surface2</h3>
+					<h3 class="h3">surface2</h3>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
 						gravida odio porttitor sem non mi integer non faucibus
@@ -47,7 +65,7 @@ export default component$(() => {
 					</p>
 				</div>
 				<div class="column large-4 huge-4 surface3">
-					<h4 class="h3">surface3</h4>
+					<h3 class="h3">surface3</h3>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
 						gravida odio porttitor sem non mi integer non faucibus
@@ -59,6 +77,20 @@ export default component$(() => {
 						accumsan.
 					</p>
 				</div>
+				<div class="column large-4 huge-4 surface4">
+					<h3 class="h3">surface4</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+
 				<div class="column large-4 huge-4 surface4">
 					<h5 class="h4">surface4</h5>
 					<p>
@@ -124,6 +156,58 @@ export default component$(() => {
 						accumsan.
 					</p>
 				</div>
+				<div class="column large-4 huge-4 primary">
+					<h3 class="h2">primary</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+				<div class="column large-4 huge-4 secondary">
+					<h3 class="h3">secondary</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+				<div class="column large-4 huge-4 tertiary">
+					<h3 class="h3">tertiary</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+				<div class="column large-4 huge-4 quaternary">
+					<h3 class="h3">quaternary</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
 				<div class="column large-4 huge-4 surface1">
 					<h3 class="h2">surface1</h3>
 					<p>
@@ -137,7 +221,7 @@ export default component$(() => {
 						accumsan.
 					</p>
 				</div>
-				<div class="column large-4 huge-4 surface2">
+				<div class="column large-4 huge-4 surface5">
 					<h3 class="h3">surface2</h3>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
@@ -150,7 +234,7 @@ export default component$(() => {
 						accumsan.
 					</p>
 				</div>
-				<div class="column large-4 huge-4 surface3">
+				<div class="column large-4 huge-4 surface2">
 					<h3 class="h3">surface3</h3>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
@@ -165,6 +249,33 @@ export default component$(() => {
 				</div>
 				<div class="column large-4 huge-4 surface4">
 					<h3 class="h3">surface4</h3>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+
+				<div class="column large-4 huge-4 surface6">
+					<h5 class="h4">surface5</h5>
+					<p>
+						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
+						gravida odio porttitor sem non mi integer non faucibus
+						ornare mi ut ante amet placerat aliquet. Volutpat
+						commodo eu sed ante lacinia. Sapien a lorem in integer
+						ornare praesent commodo adipiscing arcu in massa commodo
+						lorem accumsan at odio massa ac ac. Semper adipiscing
+						varius montes viverra nibh in adipiscing blandit tempus
+						accumsan.
+					</p>
+				</div>
+				<div class="column large-4 huge-4 surface8">
+					<h5 class="h4">surface5</h5>
 					<p>
 						Nunc lacinia ante nunc ac lobortis. Interdum adipiscing
 						gravida odio porttitor sem non mi integer non faucibus
